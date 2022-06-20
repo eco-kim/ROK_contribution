7c2043bd0b043f99540ded14a7664c536ee64aa9
eco-kim <ikho768@gmail.com>
2022-06-20 16:05:35 +0900

    주석

--- power_invest/config.py
+++ power_invest/config.py
@@ -1,7 +1,8 @@
 
-basedir = 'C:/Users/ikho7/Desktop/projects/rok_contribution/power_invest/'
-datadir = 'C:/Users/ikho7/Desktop/projects/rok_contribution/data/'
-noxdir = 'D:/Program Files/Nox/bin/'
-res = 1600
-kdnum = 2038
-maxrank = 990
+basedir = #source 디렉토리
+datadir = #데이터 저장할 곳
+noxdir = #녹스 실행파일 있는곳, 예 : 'D:/Program Files/Nox/bin/'
+res = 1600 ##인티저로 작성
+kdnum = 2038 ##인티저로 작성
+maxrank = 990 ##몇위까지 조사할건지
+dbadress = ##'user:passwd@TCP/IP:port/db'
